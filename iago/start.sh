8151fb50dbc6ebf5e5c210e02be58e444678cd91
Sulaiman Ghori <skghori03@gmail.com>
2021-10-12 02:51:56 +0200

    changed asgi conf

--- iago/start.sh
+++ iago/start.sh
@@ -6,6 +6,5 @@ if [ "$DEBUG" = "0" ]; then
     python /tmp/debugpy --wait-for-client --listen 0.0.0.0:5678 manage.py runserver 0.0.0.0:8000 --noreload
 else
     echo "RUNNING DJANGO START SCRIPT IN PRODUCTION"
-    curl  -fsS --retry 3 "$DUCKDNS_UPDATE" > /dev/null
     daphne -b 0.0.0.0 -p 8000 iago.asgi:application
 fi
