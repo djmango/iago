52249ea2cd5845d17b8a73da6f02f92f7941cf5d
Sulaiman Ghori <skghori03@gmail.com>
2021-11-23 15:49:15 +0100

    bumped topic model to v0.6

--- iago/v0/urls.py
+++ iago/v0/urls.py
@@ -31,29 +31,29 @@ if DEBUG:
     # https://github.com/MaartenGr/BERTopic/issues/65
     # https://github.com/scikit-learn-contrib/hdbscan/pull/495
     
-    # custom algo instances
+    # # custom algo instances
     # clusterer = hdbscan.HDBSCAN(min_cluster_size=10, metric='euclidean', prediction_data=True, cluster_selection_method='eom', core_dist_n_jobs=8)
     # embedding_model = SentenceTransformer('all-distilroberta-v1')
     # embedding_model.max_seq_length = 512
     # logger.info(f'embedding input sequence limited at {embedding_model.max_seq_length} tokens')
     
-    # logger.info('loading topic training data')
-    # with open('content.json', 'r') as f:
-    #     sentences = json.loads(f.read())
-    # random.shuffle(sentences)
-    # sentences = sentences[:100000]
+    # # logger.info('loading topic training data')
+    # # with open('content.json', 'r') as f:
+    # #     sentences = json.loads(f.read())
+    # # random.shuffle(sentences)
+    # # sentences = sentences[:100000]
 
-    # logger.info(f'topic data loaded, training with {len(sentences)}')
+    # # logger.info(f'topic data loaded, training with {len(sentences)}')
 
-    # # embed, this is expensive
-    # embeddings = embedding_model.encode(sentences, show_progress_bar=True, normalize_embeddings=True)
+    # # # embed, this is expensive
+    # # embeddings = embedding_model.encode(sentences, show_progress_bar=True, normalize_embeddings=True)
 
-    # # save embeddings
-    # with open('embeddings.pkl', "wb") as f:
-    #     pickle.dump({'sentences': sentences, 'embeddings': embeddings}, f, protocol=pickle.HIGHEST_PROTOCOL)
-    # logger.info('saved embeddings')
+    # # # save embeddings
+    # # with open('embeddings.pkl', "wb") as f:
+    # #     pickle.dump({'sentences': sentences, 'embeddings': embeddings}, f, protocol=pickle.HIGHEST_PROTOCOL)
+    # # logger.info('saved embeddings')
 
-    # Load sentences & embeddings from disc
+    # # Load sentences & embeddings from disc
     # with open('embeddings_roberta_100k.pkl', "rb") as f:
     #     stored_data = pickle.load(f)
     #     sentences = stored_data['sentences']
@@ -61,10 +61,10 @@ if DEBUG:
     # logger.info(f'embed data loaded, training with {len(sentences)}')
 
     # # topic modeling
-    # topic_model = BERTopic(nr_topics='auto', verbose=True, calculate_probabilities=False, embedding_model=embedding_model, hdbscan_model=clusterer)
+    # topic_model = BERTopic(nr_topics='auto', verbose=True, n_gram_range=(1,2), calculate_probabilities=False, embedding_model=embedding_model, hdbscan_model=clusterer)
     # topic_model.fit(sentences, embeddings=embeddings)
     # logger.info('done training topic SAVING')
-    # topic_model.save('topic_v0.5', save_embedding_model=False)
+    # topic_model.save('topic_v0.6', save_embedding_model=False)
     # logger.info('saved')
 
 # cpu is c4a.8xlarge
