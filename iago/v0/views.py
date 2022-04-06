fe16ec3bf7b58ea40ac908df443f0937614b92e1
Sulaiman Ghori <skghori03@gmail.com>
2022-04-06 08:18:43 +0200

    Added thumbnail to content skill search

--- iago/v0/views.py
+++ iago/v0/views.py
@@ -287,9 +287,9 @@ class contentSkillSearch(views.APIView):
         # search content based on the skills
         start = time.perf_counter()
         if strict:
-            content = list(ScrapedArticle.objects.filter(skills=skills).values('uuid', 'title', 'url', 'skills', 'updated_on'))
+            content = list(ScrapedArticle.objects.filter(skills=skills).values('uuid', 'title', 'url', 'skills', 'thumbnail', 'updated_on'))
         else:
-            content = list(ScrapedArticle.objects.filter(skills__in=skills).values('uuid', 'title', 'url', 'skills', 'updated_on'))
+            content = list(ScrapedArticle.objects.filter(skills__in=skills).values('uuid', 'title', 'url', 'skills', 'thumbnail', 'updated_on'))
         logger.info(f'Content search took {round(time.perf_counter() - start, 3)}s')
 
         k = request.data['k'] if 'k' in request.data else len(content)
