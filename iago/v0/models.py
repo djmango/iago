29ee3a036754163d47335eb206827db6deaa8540
Sulaiman Ghori <skghori03@gmail.com>
2022-05-29 18:31:12 +0200

    Improved model inheretance and built ImageCollection

--- iago/v0/models.py
+++ iago/v0/models.py
@@ -9,9 +9,9 @@ from v0.ai import embedding_model
 logger = logging.getLogger(__name__)
 
 
-class Topic(models.Model):
-    """ Topic object, with embedding field """
-    name = models.CharField(max_length=50, primary_key=True, editable=False)
+class StringEmbedding(models.Model):
+    """ Abstract class for string-embedding pairs """
+    name = models.CharField(max_length=255, primary_key=True, editable=False)
     embedding_all_mpnet_base_v2 = ArrayField(models.FloatField(), size=768)
 
     def create(self, name: str):
@@ -23,28 +23,38 @@ class Topic(models.Model):
     def __str__(self):
         return str(self.name)
 
-
-class Job(models.Model):
-    name = models.CharField(max_length=255, primary_key=True, editable=False)
-    embedding_all_mpnet_base_v2 = ArrayField(models.FloatField(), size=768)
-
-    def __str__(self):
-        return str(self.name)
+    class Meta:
+        abstract = True
 
 
-class Skill(models.Model):
-    name = models.CharField(max_length=255, primary_key=True, editable=False)
-    embedding_all_mpnet_base_v2 = ArrayField(models.FloatField(), size=768)
+class Topic(StringEmbedding):
+    """ Topic object, with embedding field """
+    pass
+
+
+class Job(StringEmbedding):
+    """ Job object, a job title and assosiated embedding field """
+    pass
+
+
+class Skill(StringEmbedding):
     cluster = models.ForeignKey('SkillCluster', on_delete=models.SET_NULL, blank=True, null=True)
 
-    def create(self, name: str):
-        """ Set name and generate embedding """
-        self.name = name.lower()
-        self.embedding_all_mpnet_base_v2 = list(embedding_model.model.encode(name))
-        return self
 
-    def __str__(self):
-        return str(self.name)
+class ImageCollection(StringEmbedding):
+    name = models.CharField(max_length=50, editable=False)
+    url = models.URLField(max_length=800, unique=True)
+    collection_id = models.CharField(max_length=50, editable=False) # id specific to the provider
+
+    class providers(models.TextChoices):
+        UNSPLASH = 'unsplash', 'Unsplash'
+        PEXELS = 'pexels', 'Pexels'
+        SHUTTERSTOCK = 'shutterstock', 'Shutterstock'
+
+    provider = models.CharField(
+        max_length=30,
+        choices=providers.choices
+    )
 
 
 class Content(models.Model):
@@ -54,6 +64,7 @@ class Content(models.Model):
     title = models.TextField()
     subtitle = models.TextField(blank=True, null=True)
     thumbnail = models.URLField(blank=True, null=True)
+    thumbnail_alternatives = models.JSONField(default=dict)
     content = models.TextField()
     # https://pypi.org/project/readtime/
     content_read_seconds = models.IntegerField()  # seconds = num_words / 265 * 60 + img_weight * num_images
